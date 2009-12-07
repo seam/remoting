97d8e77e868b4ad08155b9b73d68bbe48ed2244e
Shane Bryzak <sbryzak@redhat.com>
2009-12-07 15:30:05 +0000

    add persistence to example

--- examples/model/src/main/java/org/jboss/seam/remoting/examples/model/CustomerAction.java
+++ examples/model/src/main/java/org/jboss/seam/remoting/examples/model/CustomerAction.java
@@ -6,29 +6,35 @@ import javax.enterprise.context.Conversation;
 import javax.enterprise.context.ConversationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.jboss.seam.remoting.annotations.WebRemote;
 
 @ConversationScoped
 public class CustomerAction implements Serializable
 {
    private static final long serialVersionUID = 8350706339578435242L;
    
-   @Inject EntityManager entityManager;
+   @PersistenceContext EntityManager entityManager;
    @Inject Conversation conversation;
    
    private Customer customer;
       
+   @WebRemote
    public void createCustomer()
    {
       conversation.begin();
       customer = new Customer();
    }
    
+   @WebRemote
    public void editCustomer(Integer customerId)
    {
       conversation.begin();
       customer = entityManager.find(Customer.class, customerId);
    }
    
+   @WebRemote
    public void saveCustomer()
    {
       if (customer.getCustomerId() == null)
---
+++ examples/model/src/main/webapp/WEB-INF/import.sql
@@ -0,0 +1,3 @@
+insert into customer (customerid, firstname, lastname, dateofbirth, gender) values (1, 'Shane', 'Bryzak', '19010101', 1);
+
+
