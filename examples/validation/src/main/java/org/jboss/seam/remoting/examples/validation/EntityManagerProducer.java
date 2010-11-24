5eccc1b22117e5b472e31daf32a0506826770f83
Shane Bryzak <shane.bryzak@jboss.com>
2010-11-25 09:52:55 +1000

    renamed core -> impl, added empty validation example project

---
+++ examples/validation/src/main/java/org/jboss/seam/remoting/examples/validation/EntityManagerProducer.java
@@ -0,0 +1,17 @@
+package org.jboss.seam.remoting.examples.validation;
+
+import javax.enterprise.context.ConversationScoped;
+import javax.enterprise.inject.Produces;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+
+import org.jboss.seam.persistence.SeamManaged;
+
+public class EntityManagerProducer
+{
+   @PersistenceUnit
+   @ConversationScoped
+   @Produces
+   @SeamManaged
+   EntityManagerFactory emf;  
+}
